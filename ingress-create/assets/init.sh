d8ca5be786aa21ba5c084d64dcfa5b19152f5b92
Kim <kim@wuestkamp.com>
2022-04-23 09:15:50 +0200

    t

--- ingress-create/assets/init.sh
+++ ingress-create/assets/init.sh
@@ -13,6 +13,7 @@ EOT
 # scenario specific
 kubectl -f /ks/init.yaml create
 echo 172.30.1.2 world.universe.mine >> /etc/hosts
+kubectl -n ingress-nginx wait --for=condition=available --timeout=600s deployment/ingress-nginx-controller
 
 # mark init finished
 touch /ks/.initfinished
